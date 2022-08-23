790d83d0d1b7b18c9b93d0459211d75c00b1b1fc
M Yousefi <myousefi31@gmail.com>
2022-08-23 18:40:37 -0400

    Add files via upload

---
+++ PropertyListing.py
@@ -0,0 +1,85 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import pandas as pd
+
+
+class PropertyListing:
+    def __init__(self, url):
+        self.listing_url = url
+        self.property_type = None
+        self.listed_price = None
+        self.listed_address = None
+        self.building_style = None
+        self.lot_area = None
+        self.parking = None
+        self.main_unit = None
+        self.listed_potential_gross_revenue = None
+        self.listed_walk_score = None
+        self.number_of_units = None
+        self.year_built = None   
+        self.description = None
+
+    def get_data(self):
+        webdriver_path = '/Users/myouesfi/Downloads/CentrisScraper-master/chromedriver'
+        chrome_options = Options()
+        chrome_options.add_argument('--headless')
+        driver = webdriver.Chrome(webdriver_path)
+        driver.get(self.listing_url)
+        try:
+            self.property_type = driver.find_element_by_css_selector("span[data-id='PageTitle']").text
+        except:
+            pass
+        try:
+            self.listed_price = driver.find_element_by_id('BuyPrice').text
+        except:
+            pass
+        try:
+            self.listed_address = driver.find_element_by_css_selector("h2[itemprop='address']").text
+        except:
+            pass
+        try:
+            self.building_style = driver.find_element_by_xpath("//*[contains(text(), 'Building style')]/..").find_element_by_class_name('carac-value').text
+        except:
+            pass
+        try:
+            self.lot_area = driver.find_element_by_xpath("//*[contains(text(), 'Lot area')]/..").find_element_by_class_name('carac-value').text
+        except:
+            pass
+        try:
+            self.parking = driver.find_element_by_xpath("//*[contains(text(), 'Parking (total)')]/..").find_element_by_class_name('carac-value').text
+        except:
+
+            pass
+        try:
+            self.main_unit = driver.find_element_by_xpath("//*[contains(text(), 'Main unit')]/..").find_element_by_class_name('carac-value').text
+        except:
+            pass
+        try:
+            self.listed_potential_gross_revenue = driver.find_element_by_xpath("//*[contains(text(), 'Potential gross revenue')]/..").find_element_by_class_name('carac-value').text
+        except:
+            pass
+        try:
+            self.listed_walk_score = driver.find_element_by_class_name('walkscore').text
+        except:
+            pass
+        try:
+            self.number_of_units = driver.find_element_by_xpath("//*[@data-id='NbUniteFormatted']").text
+        except:
+            pass
+        try:
+            self.year_built = driver.find_element_by_xpath("//*[contains(text(), 'Year built')]/..").find_element_by_class_name('carac-value').text
+        except:
+            pass
+        try:
+            self.description = driver.find_element_by_css_selector("div[itemprop='description']").text
+        except:
+            pass
+
+        driver.close()
+
+    def to_dataframe(self):
+        df = pd.DataFrame(self.__dict__, index=[0])
+        return df
+
+
+        
---
+++ main.py
@@ -0,0 +1,41 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from PropertyListing import PropertyListing
+import time
+import pandas as pd
+
+if __name__ == "__main__":
+    try:
+
+        url_to_scrape = f'https://www.centris.ca/en/plexes~for-sale~montreal-ouest?view=Thumbnail&uc=6'
+        webdriver_path = '/Users/myouesfi/Downloads/CentrisScraper-master/chromedriver'
+        chrome_options = Options()
+        chrome_options.add_argument('--headless')
+        driver = webdriver.Chrome(webdriver_path)
+        driver.get(url_to_scrape)
+        
+        links = list()
+        last_page = False
+        while not last_page:
+            items = driver.find_elements_by_class_name('a-more-detail')
+            page_links = [item.get_attribute('href') for item in items]
+            links.extend(list(set(page_links)))
+            
+            if "inactive" in driver.find_element_by_class_name("next").get_attribute('class'):
+                break
+            driver.find_element_by_class_name('next').click()
+            time.sleep(3)
+        driver.close()
+        ## Extracting Data
+        property_list = list()
+        [property_list.append(PropertyListing(link)) for link in links]
+        [property.get_data() for property in property_list]
+
+        ## Saving Data
+        df = pd.DataFrame()
+        for property in property_list:
+            df = pd.concat([df, property.to_dataframe()])
+        df.to_csv('/Users/myouesfi/Desktop/Finalresult2.csv')
+
+    finally:
+        driver.close()
